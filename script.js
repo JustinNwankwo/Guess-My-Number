6d629aece3ccaccdfa301c3d15f50438ef0685c9
Justin Nwankwo <nuhwainks@gmail.com>
2025-05-22 10:58:30 -0400

    refactor

--- script.js
+++ script.js
@@ -2,8 +2,9 @@
 
 const number = document.querySelector('.number');
 const guess = document.querySelector('.guess');
-const message = document.querySelector(".message")
-const replay = document.querySelector(".again")
+const message = document.querySelector('.message');
+const replay = document.querySelector('.again');
+const check = document.querySelector('.check');
 
 let secretNumber;
 
@@ -13,24 +14,30 @@ const randomNumber = function () {
 
 randomNumber();
 
-guess.addEventListener('keypress', function (event) {
-  let numberGuessed = Number(guess.value)
-  if (event.key === 'Enter') {
-    if (numberGuessed === secretNumber) {
-      message.textContent = "you guessed right"
-    }
+const compareNumber = function () {
+  let numberGuessed = Number(guess.value);
 
-    else if (numberGuessed > secretNumber) {
-      message.textContent = "too high..."
-    }
-    else if (numberGuessed < secretNumber) {
-      message.textContent = "too low..."
-    }
+  if (numberGuessed === secretNumber) {
+    message.textContent = 'you guessed right';
+  } else if (numberGuessed > secretNumber) {
+    message.textContent = 'too high...';
+  } else if (numberGuessed < secretNumber) {
+    message.textContent = 'too low...';
+  }
+};
+
+check.addEventListener('click', function () {
+  compareNumber();
+});
+
+guess.addEventListener('keypress', function (event) {
+  if (event.key === 'Enter') {
+    compareNumber();
   }
 });
 
-replay.addEventListener("click", function() {
+replay.addEventListener('click', function () {
   randomNumber();
-  guess.value = ''
-  message.textContent = 'Start guessing...'
-})
+  guess.value = '';
+  message.textContent = 'Start guessing...';
+});
