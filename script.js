cd3a4ac5fab07720b63a78128debbd94d48e036d
Justin Nwankwo <nuhwainks@gmail.com>
2025-05-22 08:56:15 -0400

    attempted it on my own.

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const number = document.querySelector('.number');
+const guess = document.querySelector('.guess');
+
+const randomNumber = function () {
+  number.value = Math.round(Math.random() * 20);
+};
+
+randomNumber();
+
+guess.addEventListener('keypress', function (event) {
+  if (event.key === 'Enter') {
+    if (guess.value = number) {
+      return console.log(guess.value);
+    }
+  }
+});
+
+console.log(number.value);
+console.log(guess.value);
